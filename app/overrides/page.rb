ae16bf7798752e1e5fadf4992861d23be1ef5401
Diego Barrera <dieg_goo@hotmail.com>
2017-02-05 16:47:59 -0500

    add spree view templates

---
+++ app/overrides/page.rb
@@ -0,0 +1,13 @@
+# Deface::Override.new(virtual_path: 'spree/shared/_nav_bar',
+#                      name: 'remove-search-bar',
+#                      remove: '#search-bar')
+#
+# Deface::Override.new(virtual_path: 'spree/shared/_main_nav_bar',
+#                      name: 'remove-main-nav',
+#                      remove: '#main-nav-bar')
+
+# Deface::Override.new(virtual_path: 'spree/home/index',
+#                      replace: '[data-hook="homepage_products"]',
+#                      name: 'landing-products',
+#                      partial: 'home/landing',
+#                      original: '461aae32b5912b8551fcf3a823427507f434a0cc')
